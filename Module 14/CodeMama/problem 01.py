aa669a704aa2372084f10f7739cfac484f492b94
cepsmohi <ceps.mohi@gmail.com>
2024-11-28 20:48:56 +0600

    Thu Nov 28 20:38:00 +06 2024

---
+++ Module 14/CodeMama/problem 01.py
@@ -0,0 +1,28 @@
+"""
+Greetings
+
+Problem Statement
+Write a program that asks the user for their name and then prints a personalized greeting.
+
+Input
+The input consists of a string
+S
+S
+
+Output
+Output a single line containing the personalized greeting.
+
+Constraints
+1 ≤ S ≤ 1000
+Example:
+What is your name?
+
+Input:
+Alice
+Output:
+Hello, Alice! Nice to meet you.
+Notes:
+In the example, the user's name is "Alice". The program prints a greeting message, combining the user's name with the predefined message.
+"""
+name = input()
+print("Hello, " + name + "! Nice to meet you.")
---
+++ Module 14/CodeMama/problem 04.py
@@ -0,0 +1,26 @@
+"""
+Counting Characters
+
+Problem Statement
+Write a program that counts the number of characters in a string entered by the user.
+
+Input
+The input is a string.
+
+Output
+Output the number of characters.
+
+Constraints
+Output will be an unsigned integer.
+Example:
+Enter a string
+
+Input:
+Alice
+Output:
+5
+"""
+string = input()
+
+character_count = len(string)
+print(character_count)
